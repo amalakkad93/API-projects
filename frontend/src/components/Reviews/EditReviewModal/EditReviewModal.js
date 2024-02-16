f2242858f29b321b4ff4a0b4d246df15ebeb313d
amala <amalakkad93@outlook.com>
2024-02-15 21:37:52 -0800

    Start working on Edit Review

--- frontend/src/components/Reviews/EditReviewModal/EditReviewModal.js
+++ frontend/src/components/Reviews/EditReviewModal/EditReviewModal.js
@@ -1,94 +1,36 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
-import { updateReviewThunk } from '../../../store/reviews';
+import { useModal } from "../../../context/Modal";
+import { updateReviewThunk } from "../../../store/reviews";
 
 function EditReviewModal({ review, setReloadPage }) {
   const [updatedReview, setUpdatedReview] = useState(review.review);
   const dispatch = useDispatch();
+  const { closeModal } = useModal();
 
   const handleSubmit = async () => {
-      dispatch(updateReviewThunk(review.id, review));
-      setReloadPage(true);
+    const updatedReviewDetails = {
+      ...review,
+      review: updatedReview,
     };
-
+    dispatch(updateReviewThunk(review.id, updatedReviewDetails));
+    closeModal();
+    setReloadPage(true);
+  };
 
   return (
-    <div>
-      <textarea value={updatedReview} onChange={(e) => setUpdatedReview(e.target.value)} />
-      <button onClick={handleSubmit}>Update Review</button>
+    <div className="edit-review-modal-container">
+    <textarea
+      className="edit-review-textarea"
+      value={updatedReview}
+      onChange={(e) => setUpdatedReview(e.target.value)}
+    />
+    <div className="edit-review-modal-actions">
+      <button className="edit-review-cancel-btn" onClick={closeModal}>Cancel</button>
+      <button className="edit-review-submit-btn" onClick={handleSubmit}>Update Review</button>
     </div>
+  </div>
   );
-};
-// import { useEffect, useState } from "react";
-// import { useDispatch } from "react-redux";
+}
 
-// import { updateReviewThunk } from '../../../store/reviews';
-
-// function EditReviewModal({ review, setReloadPage }) {
-//   const [updatedReview, setUpdatedReview] = useState(review.review);
-//   const dispatch = useDispatch();
-
-//   // Update local state if the review prop changes
-//   useEffect(() => {
-//     setUpdatedReview(review.review);
-//   }, [review.review]);
-
-//   const handleSubmit = async () => {
-//       // Send only the updated review text, not the entire review object
-//       dispatch(updateReviewThunk(review.id, { review: updatedReview }));
-//       setReloadPage(true);
-//   };
-
-//   return (
-//     <div>
-//       <textarea value={updatedReview} onChange={(e) => setUpdatedReview(e.target.value)} />
-//       <button onClick={handleSubmit}>Update Review</button>
-//     </div>
-//   );
-// }
-
-// export default EditReviewModal;
-
-
-
-
-
-// import { useParams } from "react-router-dom";
-// import ReviewForm from './ReviewForm';
-// import { getAllReviewsThunk, createReviewThunk, updateReviewThunk } from "../../../store/reviews";
-
-// export default function  EditReviewForm() {
-
-//   const { reviewId } = useParams();
-//   return (
-//     <>
-//     <ReviewForm
-//       formType="Edit"
-//       reviewId={reviewId}
-//     />
-//     </>
-//     );
-// }
-
-
-
-// import ReviewForm from './ReviewForm';
-
-// import { useSelector } from 'react-redux';
-// import { getReview, editReview } from "../../../store/reviews";
-
-// const EditReviewForm = ({ reviewId, closeForm }) => {
-//   const review = useSelector(getReview(reviewId));
-
-//   return (
-//     <ReviewForm
-//       review={review}
-//       formType="Update Review"
-//       onSubmit={editReview}
-//       closeForm={closeForm}
-//     />
-//   );
-// }
-
-// export default EditReviewForm;
+export default EditReviewModal;
