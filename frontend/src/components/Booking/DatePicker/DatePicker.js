06db08ecc00440052f76e22e290789767dba9042
amala <amalakkad93@outlook.com>
2024-03-06 20:58:13 -0800

    deleted some comments

--- frontend/src/components/Booking/DatePicker/DatePicker.js
+++ frontend/src/components/Booking/DatePicker/DatePicker.js
@@ -1,98 +1,3 @@
-// import React, { useState, useMemo, useEffect } from "react";
-// import ReactDatePicker from "react-datepicker";
-// import "react-datepicker/dist/react-datepicker.css";
-// import "./DatePicker.css";
-
-// const DatePicker = ({ onDateSelect, existingBookings }) => {
-//   const [startDate, setStartDate] = useState(null);
-//   const [endDate, setEndDate] = useState(null);
-
-//   useEffect(() => {
-
-//     const bookings = existingBookings.map(booking => ({
-//       start: new Date(booking.startDate),
-//       end: new Date(booking.endDate)
-//     })).sort((a, b) => a.start - b.start);
-
-//     let firstAvailableDate = new Date();
-//     firstAvailableDate.setHours(0, 0, 0, 0);
-
-//     for (let i = 0; i < bookings.length; i++) {
-//       const booking = bookings[i];
-
-//       if (firstAvailableDate < booking.start) {
-//         break;
-//       } else if (firstAvailableDate <= booking.end) {
-
-//         firstAvailableDate = new Date(booking.end);
-//         firstAvailableDate.setDate(firstAvailableDate.getDate() + 1);
-//       }
-//     }
-
-//     setStartDate(firstAvailableDate);
-//     setEndDate(new Date(firstAvailableDate));
-//   }, [existingBookings]);
-
-//   const highlightedDates = useMemo(() => {
-//     const today = new Date();
-//     today.setHours(0, 0, 0, 0);
-
-//     return existingBookings.reduce((acc, booking) => {
-//       let currentDate = new Date(booking.startDate);
-//       const endDate = new Date(booking.endDate);
-
-//       while (currentDate <= endDate) {
-//         if (currentDate >= today) {
-//           acc.push(currentDate);
-//         }
-//         currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
-//       }
-//       return acc;
-//     }, []);
-//   }, [existingBookings]);
-
-
-//   return (
-//     <div className="date-pickers-container">
-
-//       <div className="date-picker-section">
-//         <ReactDatePicker
-//           selected={startDate}
-//           onChange={date => {
-//             setStartDate(date);
-//             onDateSelect({ startDate: date, endDate });
-//           }}
-//           startDate={startDate}
-//           endDate={endDate}
-//           selectsStart
-//           dateFormat="MM/dd/yyyy"
-//           minDate={new Date()}
-//           highlightDates={highlightedDates}
-//         />
-//       </div>
-
-//       <div className="date-picker-section">
-//         <ReactDatePicker
-//           selected={endDate}
-//           onChange={date => {
-//             setEndDate(date);
-//             onDateSelect({ startDate, endDate: date });
-//           }}
-//           startDate={startDate}
-//           endDate={endDate}
-//           selectsEnd
-//           dateFormat="MM/dd/yyyy"
-//           minDate={startDate || new Date()}
-//           highlightDates={highlightedDates}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DatePicker;
-//==================================
-
 import React, { useState, useEffect, useMemo } from "react";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -120,7 +25,7 @@ const DatePicker = ({ onDateSelect, existingBookings }) => {
       setEndDate(firstAvailableDate);
       onDateSelect({ startDate: firstAvailableDate, endDate: firstAvailableDate });
     }
-  }, [existingBookings, onDateSelect]); // Removed startDate from dependencies
+  }, [existingBookings, onDateSelect]);
 
   const highlightedDates = useMemo(() => {
     const today = new Date();
